eaee1dcbf5e72294bf9f891e84dae3c972bcb337
llwariel0705 <106910241+llwariel0705@users.noreply.github.com>
2022-08-28 23:49:03 -0400

    Add files via upload

---
+++ quiz/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "which country primarily uses english",
+    answer: "england",
+    options: [
+      "japan",
+      "england",
+      "saudi arabia",
+      "sudan "
+    ]
+  },
+    {
+    numb: 2,
+    question: "what are verbs?",
+    answer: "doing words",
+    options: [
+      "pictures",
+      "languages",
+      "symbols",
+      "doing words"
+    ]
+  },
+    {
+    numb: 3,
+    question: "reference book for english is called?",
+    answer: "dictionary",
+    options: [
+      "dictionary",
+      "bible",
+      "cow",
+      "donkey"
+    ]
+  },
+    {
+    numb: 4,
+    question: "what is 10 in words?",
+    answer: "ten",
+    options: [
+      "ninety",
+      "fifty nine",
+      "fifty",
+      "ten"
+    ]
+  },
+    {
+    numb: 5,
+    question: "what is the plural of man?",
+    answer: "men",
+    options: [
+      "men",
+      "cows",
+      "shopping",
+      "hens"
+    ]
+  },
+];
